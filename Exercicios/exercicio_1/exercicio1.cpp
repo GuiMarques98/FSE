6f1c2015018a21dc21c449d6b4a1fac7ffd8bbce
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2020-10-11 17:50:39 -0300

    adding first exercise

---
+++ Exercicios/exercicio_1/exercicio1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <string>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    // Setup
+    int uart0_filestream = -1;
+    string a = "/dev/serial0";
+    int mat = 9503;
+
+    uart0_filestream = open(a.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    if (uart0_filestream == -1)
+    {
+        cerr << "Error: Serial port not working" << endl;
+        return -1;
+    }
+
+    // Config UART
+    struct termios options;
+    options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    if (uart0_filestream == -1)
+    {
+        cerr << "Error: Serial port not working" << endl;
+        return -1;
+    }
+
+    unsigned char uart_buffer[256] = {0};
+    long int buffer = 0xA1 + mat;
+    int count = write(uart0_filestream, &buffer, sizeof(long int));
+
+    if (count <= 0)
+    {
+        cerr << "Error: Dont write in uart comunication" << endl;
+    }
+    count = 0;
+    count = read(uart0_filestream, &uart_buffer, 255);
+
+    if (count < 0)
+    {
+        cerr << "Error: Dont write in uart comunication" << endl;
+    }
+    else if (count == 0)
+    {
+        cerr << "Error: Dont read in uart comunication" << endl;
+    }
+    else
+    {
+        cout << "The uart answer is " << uart_buffer << endl;
+    }
+
+    return 0;
+}
