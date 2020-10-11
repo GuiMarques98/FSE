1a742bf027cc320fc4f430d3de840c4cbb0745b2
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2020-10-11 19:21:52 -0300

    Adding trab 1

---
+++ Trabalho/Trabalho_1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../inc/bme280.h"
+#include "../inc/bme280_defs.h"
+
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout << "Hello" << endl;
+    return 0;
+}
