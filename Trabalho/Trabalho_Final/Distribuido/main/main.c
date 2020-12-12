cdf49666058ca830a1bc4620eebbd19440739c7b
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2020-12-12 18:58:42 -0300

    adding project structure

---
+++ Trabalho/Trabalho_Final/Distribuido/main/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+#include "driver/gpio.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#include "dht11.h"
+
+void app_main(void)
+{
+    DHT11_init(4);
+
+    while (1)
+    {
+        printf("Temperature is %d \n", DHT11_read().temperature);
+        printf("Humidity is %d\n", DHT11_read().humidity);
+        printf("Status code is %d\n", DHT11_read().status);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
